1f7248fdff3221317d41ded620c668ea2ff0d06f
souza99 <jpfsouza99@gmail.com>
2022-06-28 18:40:03 -0300

    Calculo do empacotamento

--- lib/aula/frete.dart
+++ lib/aula/frete.dart
@@ -1,24 +1,46 @@
 main() {
   var frete = Frete(comprmentoPacote: 30, larguraPacote: 60, alturaPacote: 90);
-  double resultadoPacotamento = (frete.alturaPacote * frete.comprmentoPacote) *
-      (frete.larguraPacote / 6000);
+  double resultadoPacotamento = (frete._alturaPacote * frete._comprmentoPacote) *
+      (frete._larguraPacote / 6000);
 
   print(resultadoPacotamento);
 }
 
 class Frete {
-  final double comprmentoPacote;
-  final double larguraPacote;
-  final double alturaPacote;
+  late  double _comprmentoPacote;
+  late  double _larguraPacote;
+  late  double _alturaPacote;
 
   Frete(
-      {required this.alturaPacote,
-      required this.comprmentoPacote,
-      required this.larguraPacote}) {
-    if (this.alturaPacote > 0 &&
-        this.comprmentoPacote > 0 &&
-        this.larguraPacote > 0) {
+      {required alturaPacote,
+      required comprmentoPacote,
+      required larguraPacote}) {
+    if (alturaPacote > 0 &&
+        comprmentoPacote > 0 &&
+        larguraPacote > 0) {
       print('Tudo pronto para o calculo');
     }
   }
+
+  double get comprmentoPacote => _comprmentoPacote;
+
+  set comprmentoPacote(double complementoPacote) => this._comprmentoPacote;
+
+  double get larguraPacote => _larguraPacote;
+
+  set larguraPacote(double larguraPacote) => this._larguraPacote;
+
+
+  double get alturaPacote => _alturaPacote;
+
+  set alturaPacote(double alturaPacote) => this._alturaPacote;
+
+
+  double calculaEmpacotamento(Frete frete){
+    double resultadoPacotamento = (frete._alturaPacote * frete._comprmentoPacote) *
+      (frete._larguraPacote / 6000);
+
+      return resultadoPacotamento;
+  }
+
 }
